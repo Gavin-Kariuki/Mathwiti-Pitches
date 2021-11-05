1606001947ee008d6653dccf96ba6a2980337ea8
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-05 23:32:03 +0300

    add various changes to the files

---
+++ app/auth/forms.py
@@ -0,0 +1,6 @@
+from flask_wtf import FlaskForm
+from wtforms import ValidationError, StringField, PasswordField, SubmitField, BooleanField
+from ..models import User
+
+class LoginForm(FlaskForm):
+    
--- app/main/errors.py
+++ app/main/errors.py
@@ -4,5 +4,5 @@ from . import main
 @main.app_errorhandler(404)
 def four_Ow_four(error):
     '''Function to render the 404 error page'''
-
-    return render_template('fourOwfour.html'),404
+    title = '404 ERROR'
+    return render_template('fourOwfour.html', title = title),404
