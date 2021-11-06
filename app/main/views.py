3ba85312c1cb40300787599b094644ba950dec5e
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-06 17:51:19 +0300

    add content for base.html

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,18 @@
 from flask import render_template, request, redirect, url_for, abort, flash
 from . import main
+from . import auth
 from ..models import User, Pitch, Comment, Upvote, Downvote
 from .. import db,photos
 from flask_login import login_required, current_user
 from .forms import PitchForm, UpdateProfile, CommentForm
 
+@main.route('/')
+def main_page():
+    '''this is my main page where a user will either login or register to access the site'''
+
+    return redirect(url_for('auth.login'))
+
+@main.route('/login')
+def landing_page():
+    title = 'Mathwiti | Login'
+    return render_template('index.html', title = title)
