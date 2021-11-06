cd2817a81749792c4f9ed9d5958b3780f73dfd9a
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-06 23:09:49 +0300

    update forms.py in auth

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -8,7 +8,7 @@ class RegistrationForm(FlaskForm):
     username = StringField('Enter your username',validators=[Required()])
     password = PasswordField('Password',validators=[Required(),EqualTo('password_confirm',message = 'Passwords must match')])
     remember = BooleanField('Remember me')
-    password_confirm = PasswordField('Confirm Passwords',validators = [Required()])
+    password_confirm = PasswordField('Confirm Password',validators = [Required()])
     submit = SubmitField('Sign Up')
 
     def validate_email(self, data_field):
