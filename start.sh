3c57e13048d806026c35aca54efd905364519418
Gavin Kariuki <gavin.kariuki@student.moringaschool.com>
2021-11-08 19:35:14 +0300

    fresh

--- start.sh
+++ start.sh
@@ -1,9 +1,5 @@
-export SECRET_KEY='choomba'
-export WTF_CSRF_SECRET_KEY='powerful secret key'
 export MAIL_USERNAME='gavin.kariuki@student.moringaschool.com'
+export MAIL_PASSWORD='Mzabibu21'
 
 
-python manage.py db init
-python manage.py db migrate
-python manage.py db upgrade
-python manage.py server --port 8888
+python3 manage.py server
